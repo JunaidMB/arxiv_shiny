ef9ae14617cdc4909f91e87b5e8b2bcada51cf8f
Junaid Butt <junaidbutt@RMB-Pro.local>
2020-10-02 15:03:15 +0100

    added a plumber file

---
+++ database_update/plumber.R
@@ -0,0 +1,93 @@
+library(jsonlite)
+library(bigrquery)
+library(DBI)
+library(aRxiv)
+library(lubridate)
+library(dplyr)
+library(glue)
+library(dotenv)
+library(tidyverse)
+library(stringi)
+library(stringr)
+library(plumber)
+
+
+
+#* Check token - Request body should contain a token to authenticate HTTP request
+#* @filter checkAuth
+function(req, res){
+  
+  request <- jsonlite::fromJSON(req$postBody)
+  
+  if(request$token != Sys.getenv('plumber_token_auth')) {
+    
+    return(list(message = "token is incorrect"))
+    
+  } else {
+    plumber::forward()
+  }
+}
+
+#* Update arxiv repository table in BigQuery -- Batch Deployment
+#* @post /update
+function(){
+  # Authentication for BQ
+  bigrquery::bq_auth(path = Sys.getenv('auth_path'))
+  
+  
+  # Connect to DB
+  bq_con <- DBI::dbConnect(bigquery(),
+                           project = Sys.getenv('project_id'),
+                           dataset = Sys.getenv('dataset_id'))
+  
+  # Pull down DB table
+  
+  
+  # Obtain latest days papers for all categories
+  select_choices <- aRxiv::arxiv_cats$abbreviation
+  names(select_choices) <- aRxiv::arxiv_cats$description
+  
+  # Set and format dates for the arxiv API
+  sql <- "select max(date(submitted)) as date from arxiv_paper_repo.arxiv_paper_repo"
+  
+  last_db_day <- lubridate::as_date(bigrquery::bq_table_download(bigrquery::bq_project_query(x = Sys.getenv('project_id'), query = sql), max_results = Inf)$date)
+  today <- Sys.Date()
+  
+  formatted_last_db_day <- stringr::str_c(stringr::str_c(gsub("-", "", last_db_day) , "*"))
+  formatted_today <- stringr::str_c(stringr::str_c(gsub("-", "", Sys.Date()) , "*"))
+  
+  # Update table for all categories
+  today_results <- data.frame()
+
+  for (i in seq_along(select_choices)) {
+
+    results_today <- try(as_tibble(aRxiv::arxiv_search(query = glue::glue("cat: ({select_choices[[i]]}) AND submittedDate: [{formatted_last_db_day} TO {formatted_today}]")
+                                                       , limit = 10000
+                                                       , sort_by = c("submitted")
+                                                       , ascending = FALSE
+                                                       , batchsize = 10000)), silent = TRUE)
+
+    today_results <- dplyr::union_all(today_results, results_today)
+
+    cat(i, names(select_choices)[i], "\n")
+
+  }
+
+  # Format date
+  today_results <- today_results %>%
+    dplyr::mutate(submitted = lubridate::as_datetime(submitted),
+                  updated = lubridate::as_datetime(updated))
+
+  # Write the table back to the database
+  DBI::dbWriteTable(conn = bq_con,
+                    name = 'arxiv_paper_repo.arxiv_paper_repo',
+                    value = today_results,
+                    as_bq_fields(today_results),
+                    overwrite = FALSE,
+                    append = TRUE)
+
+  # Close connection
+  DBI::dbDisconnect(conn = bq_con)
+  
+  return(list(message = "Update Successful"))
+}
